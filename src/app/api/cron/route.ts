101d586277b50d6862757c27e6627de384378377
Dannirl6378 <127243165+Dannirl6378@users.noreply.github.com>
2025-09-22 20:24:45 +0200

    feat: nastavovani cron

--- src/app/api/cron/route.ts
+++ src/app/api/cron/route.ts
@@ -6,20 +6,20 @@ const supabase = createClient(
   process.env.SUPABASE_SERVICE_ROLE_KEY! // musíš použít Service Role Key
 );
 
-export async function GET() {
+export async function DELETE() {
   // smaže všechny joby starší než 1 den
   await supabase
-    .from("jobs")
+    .from("Job")
     .delete()
     .eq("isDemo", true)
-    .lt("created_at", new Date(Date.now() - 24 * 60 * 60 * 1000).toISOString());
+    .lt("createdat", new Date(Date.now() - 24 * 60 * 60 * 1000).toISOString());
 
   // smaže všechny uživatele starší než 1 den
   await supabase
-    .from("users")
+    .from("User")
     .delete()
     .eq("isDemo", true)
-    .lt("created_at", new Date(Date.now() - 24 * 60 * 60 * 1000).toISOString());
+    .lt("createdat", new Date(Date.now() - 24 * 60 * 60 * 1000).toISOString());
 
   return NextResponse.json({ status: "ok", message: "Old demo data deleted" });
 }
